bdf9c3c55dae02a45e60d704b6f39c8deba3e18b
Pedro Gomes <pedrogomes94@gmail.com>
2018-12-29 21:33:45 +0000

    add dimensions

---
+++ src/helpers/dimensions.ts
@@ -0,0 +1,9 @@
+import { Platform, StatusBar, Dimensions } from "react-native";
+
+export const STATUS_BAR_HEIGHT =
+  Platform.OS === "ios" ? 20 : StatusBar.currentHeight;
+export const WINDOW_WIDTH = Dimensions.get("window").width;
+export const WINDOW_HEIGHT = Dimensions.get("window").height;
+export const WINDOW_HEIGHT_NO_STATUS_BAR = STATUS_BAR_HEIGHT
+  ? WINDOW_HEIGHT - STATUS_BAR_HEIGHT
+  : WINDOW_HEIGHT;
